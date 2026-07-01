8e684d46288ab52e640d1dc912d6a2cf739faa35
b-sn <117035335+b-sn@users.noreply.github.com>
2026-07-01 19:05:25 +0200

    Add installation script for LogSeq

---
+++ install_logseq.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if command -v logseq >/dev/null 2>&1; then
+	echo "LogSeq already installed"
+  exit
+fi
+
+curl -fsSL https://raw.githubusercontent.com/logseq/logseq/master/scripts/install-linux.sh | bash
