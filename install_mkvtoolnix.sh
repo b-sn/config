b9743eb3d10e0dd0976180b21717d3373c2c17d1
b-sn <117035335+b-sn@users.noreply.github.com>
2026-07-01 19:34:42 +0200

    Update install_mkvtoolnix.sh

--- install_mkvtoolnix.sh
+++ install_mkvtoolnix.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install wget
+if command -v mkvtoolnix >/dev/null 2>&1; then
+  echo "MkvToolNix already installed"
+  exit
+fi
+
+if ! command -v wget >/dev/null 2>&1; then
+  sudo apt-get update
+  sudo apt-get -y install wget
+fi
+
 sudo wget -O /etc/apt/keyrings/gpg-pub-moritzbunkus.gpg https://mkvtoolnix.download/gpg-pub-moritzbunkus.gpg
 
 VERSION=$(grep '^VERSION_CODENAME=' /etc/os-release | cut -d= -f2)
