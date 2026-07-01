ab98b32a31605cc8be7322d694a8050fca3c5cfa
b-sn <117035335+b-sn@users.noreply.github.com>
2026-07-01 19:28:59 +0200

    Enhance install script with Zellij check
    Added checks for Zellij installation and Rust installation.

--- install_zellij.sh
+++ install_zellij.sh
@@ -2,9 +2,14 @@
 
 # Source: https://zellij.dev/documentation/installation.html#third-party-repositories
 
-if ! command -v cargo >/dev/null 2>&1; then
-	echo "Install Rust first"
+if command -v zellij >/dev/null 2>&1; then
+  echo "Zellij already installed"
   exit
+fi
+
+if ! command -v cargo >/dev/null 2>&1; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  . "$HOME/.cargo/env"
 fi
 
 cargo install --locked zellij
