188af59a822872472dc8651fca45345493171a97
b-sn <117035335+b-sn@users.noreply.github.com>
2026-07-05 22:42:14 +0200

    Add installation script for smartmontools

---
+++ install_smartmontools.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Source: https://www.smartmontools.org/wiki/Download
+
+sudo apt-get update
+sudo apt-get -y intsll git automake libtool
+
+git clone https://github.com/smartmontools/smartmontools
+
+cd smartmontools
+./autogen.sh
+./configure
+make
+sudo make install
+cd ../
+rm -fR smartmontools
+
+sudo /usr/sbin/update-smart-drivedb
