d5285217580357372f5a913b3626eb554e29cfba
b-sn <117035335+b-sn@users.noreply.github.com>
2026-07-14 08:32:42 +0200

    Add script to install PostgreSQL 18

---
+++ install_postgresql.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sudo apt install -y curl ca-certificates
+sudo install -d /usr/share/postgresql-common/pgdg
+sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+
+sudo cat > /etc/apt/sources.list.d/pgdg.sources << EOF
+Types: deb deb-src
+URIs: https://apt.postgresql.org/pub/repos/apt
+Suites: trixie-pgdg
+Architectures: amd64
+Components: main
+Signed-By: /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc
+EOF
+
+sudo apt update
+sudo apt install postgresql-18
